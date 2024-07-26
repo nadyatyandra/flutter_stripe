6c950db6d291cb7e8edecf67e814ea18938d6674
Ghazi naqvi <89378614+ghazinaqvi@users.noreply.github.com>
2024-07-26 16:49:50 +0500

    chore: dependency fix for web (#5)
    * chore: dependency fix for web
    
    * fix imports
    
    * contructor fix

---
+++ packages/stripe_web/lib/flutter_stripe.dart
@@ -0,0 +1,4 @@
+library stripe_web;
+
+export 'flutter_stripe_web_stub.dart'
+    if (dart.library.js) 'flutter_stripe_web.dart';
--- packages/stripe_web/lib/flutter_stripe_web.dart
+++ packages/stripe_web/lib/flutter_stripe_web.dart
@@ -1,5 +1,3 @@
-library stripe_web;
-
 export 'package:stripe_js/stripe_api.dart' show ConfirmPaymentParams;
 export 'package:stripe_platform_interface/stripe_platform_interface.dart';
 
